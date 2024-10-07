8dcde92dcc97867a9225e2817d494a4b77196e88
Amanda Lang <Cadenc2003@icloud.com>
2024-10-07 02:53:57 -0500

    created helper functions, set up GET and POST routes for /api/notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,32 @@
 const notes = require('express').Router()
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const ShortUniqueId = require('short-unique-id')
+const { sequentialUUID } = new ShortUniqueId()
+
+// GET route to retrieve all notes
+notes.get('/', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+)
+
+// POST route for adding new notes
+notes.post('/', (req, res) => {
+    console.log(req.body)
+    const { noteTitle, noteText } = req.body
+
+    const newNote = {
+        noteTitle,
+        noteText,
+        noteID: sequentialUUID()
+    }
+
+    readAndAppend(newNote, './db/db.json')
+
+    const response = {
+        status: 'success',
+        body: newNote,
+    }
+
+    res.json(response)
+})
 
 module.exports = notes
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const path = require('path')
-const api = require('./routes/index.html')
+const api = require('./routes/index.js')
 
 const PORT = process.env.port || 3001
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('/api', api)
 
 app.get('/', (req, res) =>
